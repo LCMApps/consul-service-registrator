213d6d9a77e5d418f41dc7793283c0c3749201be
Dmitry Menshikov <d.menshikov@gmail.com>
2016-08-01 18:22:01 +0300

    Version bump. 1.1.0
    - Passing of options for consul (host, port, secure) instead of passing consul object
    - Logic of registration of checks was changed. Bug in node consul lib makes mass registration of checks impossible,
      so the first action of this lib is registration of service and then registration of checks
    - addHttpCheck method can register checks after service registration
    - addHttpCheck method returns promise

--- test/ServiceRegistrator.js
+++ test/ServiceRegistrator.js
@@ -1,16 +1,25 @@
 'use strict';
 
+/* jshint loopfunc:true */
+
 const assert = require('chai').assert;
 const sinon = require('sinon');
+const async_ = require('asyncawait/async');
+const await_ = require('asyncawait/await');
 const randomstring = require('randomstring');
 const ServiceRegistrator = require('../lib/ServiceRegistrator');
 
 const errorMessages = {
-  MUST_BE_OBJECT: 'argument must be an object',
-  MUST_BE_CONSUL: 'argument must be an instance of Consul class',
+  MUST_BE_OBJECT: 'options must be an object',
+  MUST_BE_CONSUL: 'options must have host and port fields',
   MUST_BE_PROMISIFIED: 'consul must be created with promisify option',
   ADDRESS_TYPE: 'address must be a string',
   ADDRESS_NOT_IPV4: 'address must be an IPv4 IP address',
+};
+
+const CONSUL_OPTIONS = {
+  host: '127.0.0.1',
+  port: 8500,
 };
 
 function getFakeConsulObject(promisify) {
@@ -22,6 +31,9 @@ function getFakeConsulObject(promisify) {
       service: {
         register: () => {},
         deregister: () => {},
+      },
+      check: {
+        register: () => {},
       },
     },
   };
@@ -44,49 +56,34 @@ describe('ServiceRegistrator', function () {
 
     it('argument is an object but without properties', () => {
       // this is an object and it should pass this check but fail with another text of Error
-      let consul = {};
-      assert.throws(() => { new ServiceRegistrator(consul, 'name'); }, Error, errorMessages.MUST_BE_CONSUL);
+      let consulOptions = {};
+      assert.throws(() => { new ServiceRegistrator(consulOptions, 'name'); }, Error, errorMessages.MUST_BE_CONSUL);
     });
 
-    it('argument is a Consul object without promisify option', () => {
-      let consul = { _opts: {} };
-      assert.throws(() => { new ServiceRegistrator(consul, 'name'); }, Error, errorMessages.MUST_BE_CONSUL);
+    it('argument is a Consul object with incorect type of host (string) option', () => {
+      let consulOptions = { host: {} };
+      assert.throws(() => { new ServiceRegistrator(consulOptions, 'name'); }, Error, errorMessages.MUST_BE_CONSUL);
     });
 
-    it('argument is a Consul object with promisify option but incorrect type', () => {
-      let consul = getFakeConsulObject(false);
-      consul._opts.promisify = 123;
-
-      assert.throws(() => { new ServiceRegistrator(consul, 'name'); }, Error, errorMessages.MUST_BE_PROMISIFIED);
+    it('argument is a Consul object with incorect type of port (int) option', () => {
+      let consulOptions = { port: {} };
+      assert.throws(() => { new ServiceRegistrator(consulOptions, 'name'); }, Error, errorMessages.MUST_BE_CONSUL);
     });
 
-    it('argument is a Consul object with promisify off', () => {
-      let consul = getFakeConsulObject(false);
-
-      assert.throws(() => { new ServiceRegistrator(consul, 'name'); }, Error, errorMessages.MUST_BE_PROMISIFIED);
-    });
-
-    it('argument is a Consul object with promisify', () => {
-      let consul = getFakeConsulObject(true);
-
-      assert.doesNotThrow(() => { new ServiceRegistrator(consul, 'name'); });
-    });
   });
 
   describe('#setters', function () {
     it('check serviceName', () => {
-      let consul = getFakeConsulObject(true);
       let serviceName = randomstring.generate();
       let service;
 
-      assert.doesNotThrow(() => { service = new ServiceRegistrator(consul, serviceName); });
+      assert.doesNotThrow(() => { service = new ServiceRegistrator(CONSUL_OPTIONS, serviceName); });
       assert.equal(service.getServiceName(), serviceName);
     });
 
     describe('setAddress with invalid type', function () {
       let incorrectArguments = [42, true, null, undefined, Symbol(), () => {}, {}];
-      let consul = getFakeConsulObject(true);
-      let service = new ServiceRegistrator(consul, 'name');
+      let service = new ServiceRegistrator(CONSUL_OPTIONS, 'name');
       let testSetAddressFn = function testSetAddress(arg) {
         return () => {service.setAddress(arg);};
       };
@@ -100,8 +97,7 @@ describe('ServiceRegistrator', function () {
 
     describe('setAddress with valid type and incorrect IP V4 address', function () {
       let incorrectArguments = ['', '256.0.0.0', '-1.0.0.0', '255.0.256.0', '1.1.1.p2', '0000', 'fe80::5efe:c0a8:33c'];
-      let consul = getFakeConsulObject(true);
-      let service = new ServiceRegistrator(consul, 'name');
+      let service = new ServiceRegistrator(CONSUL_OPTIONS, 'name');
       let testSetAddressFn = function testSetAddress(arg) {
         return () => { service.setAddress(arg); };
       };
@@ -115,7 +111,8 @@ describe('ServiceRegistrator', function () {
 
     it('setAddress with valid argument', function () {
       let consul = getFakeConsulObject(true);
-      let service = new ServiceRegistrator(consul, 'name');
+      let service = new ServiceRegistrator(CONSUL_OPTIONS, 'name');
+      service._consul = consul;
       service.setAddress('127.0.0.1');
     });
 
@@ -144,24 +141,6 @@ describe('ServiceRegistrator', function () {
           },
           arg: { name: 'name', id: 'name.' + pid, tags: ['tag1', 'tag2'] },
         },
-        {
-          init: (service) => {
-            service.addHttpCheck('health', 'Healthcheck over HTTP API', 'http://1.2.3.4:12345/', '1s', '100ms');
-          },
-          arg: {
-            name: 'name',
-            id: 'name.' + pid,
-            checks: [
-              {
-                id: 'health',
-                name: 'Healthcheck over HTTP API',
-                http: 'http://1.2.3.4:12345/',
-                interval: '1s',
-                timeout: '100ms',
-              },
-            ],
-          },
-        },
       ];
       let callNo = 1;
 
@@ -170,7 +149,8 @@ describe('ServiceRegistrator', function () {
         let arg = test.arg;
         it('test #' + callNo, function () {
           let consul = getFakeConsulObject(true);
-          let service = new ServiceRegistrator(consul, 'name');
+          let service = new ServiceRegistrator(CONSUL_OPTIONS, 'name');
+          service._consul = consul;
           init(service);
 
           let consulRegisterStub = sinon.stub(consul.agent.service, 'register');
@@ -187,6 +167,208 @@ describe('ServiceRegistrator', function () {
         });
 
         callNo++;
+      });
+    });
+
+    describe('register: extended cases', function () {
+      function _checksRegister(consul, amountOfChecks) {
+        let data = [];
+        let consulCheckRegisterStub = sinon.stub(consul.agent.check, 'register');
+        for (let i = 0; i < amountOfChecks; i++) {
+          let resolvePromise;
+          let rejectPromise;
+          let checkRegPromise = new Promise((resolve, reject) => {
+            resolvePromise = resolve;
+            rejectPromise = reject;
+          });
+
+          consulCheckRegisterStub.onCall(i).returns(checkRegPromise);
+          data.push({
+            resolve: resolvePromise,
+            reject: rejectPromise,
+            stub: consulCheckRegisterStub,
+          });
+        }
+
+        return data;
+      }
+
+      it('error on register without checks, from consul', function (done) {
+        let consul = getFakeConsulObject(true);
+        let service = new ServiceRegistrator(CONSUL_OPTIONS, 'name');
+        service._consul = consul;
+
+        let resolveRegPromise;
+        let rejectRegPromise;
+        let regPromise = new Promise((resolve, reject) => {
+          resolveRegPromise = resolve;
+          rejectRegPromise = reject;
+        });
+
+        let consulRegisterStub = sinon.stub(consul.agent.service, 'register');
+        consulRegisterStub.returns(regPromise);
+        let regResult = service.register();
+
+        assert.equal(consulRegisterStub.callCount, 1, 'must be called once');
+        assert.strictEqual(regResult, regPromise);
+
+        let regError = new Error('regerr');
+
+        regResult.catch(err => {
+          assert.strictEqual(err, regError);
+          done();
+        });
+
+        rejectRegPromise(regError);
+      });
+
+      it('register without checks, and then add some checks', function (done) {
+        async_(() => {
+          let consul = getFakeConsulObject(true);
+          let service = new ServiceRegistrator(CONSUL_OPTIONS, 'name');
+          service._consul = consul;
+
+          let regPromise = Promise.resolve();
+          let consulRegisterStub = sinon.stub(consul.agent.service, 'register');
+          consulRegisterStub.returns(regPromise);
+          let checkRegPromises = _checksRegister(consul, 1);
+          let firstCheckRegData = checkRegPromises[0];
+
+          assert.isNull(service._checks);
+          assert.isFalse(service._active);
+
+          await_(service.register());
+
+          assert.equal(consulRegisterStub.callCount, 1, 'must be called once');
+
+          assert.isTrue(service._active);
+
+          let checkRegPromise = service.addHttpCheck('checkid', 'checkname', 'http://localhost:8080', '10s', '30s');
+
+          firstCheckRegData.resolve();
+          await_(checkRegPromise);
+
+          let checkRegArgs = {
+            id: 'checkid',
+            serviceid: service._serviceId,
+            name: 'checkname',
+            http: 'http://localhost:8080',
+            interval: '10s',
+            ttl: '30s',
+          };
+
+          assert.equal(firstCheckRegData.stub.callCount, 1, 'must be called once');
+          assert.isTrue(
+            firstCheckRegData.stub.firstCall.calledWith(checkRegArgs),
+            firstCheckRegData.stub.printf('incorrect argument, calls: %C')
+          );
+          assert.isArray(service._checks);
+          assert(service._checks.length, 1);
+          assert.strictEqual(JSON.stringify(service._checks[0]), JSON.stringify(checkRegArgs));
+        })().then(done).catch(done);
+      });
+
+      it('register with checks, that fails', function (done) {
+        async_(() => {
+          let consul = getFakeConsulObject(true);
+          let service = new ServiceRegistrator(CONSUL_OPTIONS, 'name');
+          service._consul = consul;
+
+          let regPromise = Promise.resolve();
+          let consulRegisterStub = sinon.stub(consul.agent.service, 'register');
+          consulRegisterStub.returns(regPromise);
+          let checkRegPromises = _checksRegister(consul, 1);
+          let firstCheckRegData = checkRegPromises[0];
+
+          assert.isNull(service._checks);
+          assert.isFalse(service._active);
+
+          let checkRegArgs = {
+            id: 'checkid',
+            serviceid: service._serviceId,
+            name: 'checkname',
+            http: 'http://localhost:8080',
+            interval: '10s',
+            ttl: '30s',
+          };
+          service.addHttpCheck('checkid', 'checkname', 'http://localhost:8080', '10s', '30s');
+
+          assert.isArray(service._checks);
+          assert.equal(service._checks.length, 1);
+          assert.strictEqual(JSON.stringify(service._checks[0]), JSON.stringify(checkRegArgs));
+          assert.isFalse(service._active);
+
+          let deregisterStub = sinon.stub(service, 'deregister');
+          deregisterStub.returns(Promise.resolve());
+
+          let promise = service.register();
+
+          assert.equal(consulRegisterStub.callCount, 1, 'must be called once');
+          assert.isTrue(service._active);
+
+          firstCheckRegData.reject(new Error('check reg error'));
+
+          assert.throws(
+            () => { await_(promise); },
+            /^Can not register one of checks for the service `\w+\.\d+`, failed with error: Error: check reg error$/
+          );
+
+          assert.equal(deregisterStub.callCount, 1, 'must be called once');
+          assert.isTrue(deregisterStub.firstCall.calledWith());
+        })().then(done).catch(done);
+      });
+
+      it('register with checks, that fails and deregister that fails', function (done) {
+        async_(() => {
+          let consul = getFakeConsulObject(true);
+          let service = new ServiceRegistrator(CONSUL_OPTIONS, 'name');
+          service._consul = consul;
+
+          let regPromise = Promise.resolve();
+          let consulRegisterStub = sinon.stub(consul.agent.service, 'register');
+          consulRegisterStub.returns(regPromise);
+          let checkRegPromises = _checksRegister(consul, 1);
+          let firstCheckRegData = checkRegPromises[0];
+
+          assert.isNull(service._checks);
+          assert.isFalse(service._active);
+
+          let checkRegArgs = {
+            id: 'checkid',
+            serviceid: service._serviceId,
+            name: 'checkname',
+            http: 'http://localhost:8080',
+            interval: '10s',
+            ttl: '30s',
+          };
+          service.addHttpCheck('checkid', 'checkname', 'http://localhost:8080', '10s', '30s');
+
+          assert.isArray(service._checks);
+          assert.equal(service._checks.length, 1);
+          assert.strictEqual(JSON.stringify(service._checks[0]), JSON.stringify(checkRegArgs));
+          assert.isFalse(service._active);
+
+          let deregisterStub = sinon.stub(consul.agent.service, 'deregister');
+          deregisterStub.returns(Promise.reject('dereg fail'));
+
+          let promise = service.register();
+
+          assert.equal(consulRegisterStub.callCount, 1, 'must be called once');
+          assert.isTrue(service._active);
+
+          firstCheckRegData.reject(new Error('check reg error'));
+
+          assert.throws(
+            () => { await_(promise); },
+            new RegExp(
+              '^Can not register one of checks for the service `\\w+\\.\\d+`, failed with error: ' +
+                'Error: check reg error and failed to deregister just started service due to error: `dereg fail`$'
+            )
+          );
+
+          assert.equal(deregisterStub.callCount, 1, 'must be called once');
+          assert.isTrue(deregisterStub.firstCall.calledWith());
+        })().then(done).catch(done);
       });
     });
   });
