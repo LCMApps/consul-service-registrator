f61b80e90c5158e51c78166879247339add19ebf
Dmitry Menshikov <d.menshikov@gmail.com>
2016-08-04 15:08:40 +0300

    Missed export in index.js

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 
 exports.Registrator = require('./lib/ServiceRegistrator');
 exports.IpAddressDetector = require('./lib/IpAddressDetector');
+exports.ServiceObserver = require('./lib/ServiceObserver');
